b7ba379acc6fc853db1f792098d7122bead8c19e
ppearce05 <ppearce05@Josephs-Mac.local>
2020-09-21 00:01:34 -0700

    add Pint Pour component

---
+++ src/components/PintPour.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import Keg from './Keg';
+
+function PintPour(props) {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    )
+
+}
+
+PintPour.propTypes = {
+    handlePintPour: PropTypes.func,
+    
+}
+
+export default PintPour;
